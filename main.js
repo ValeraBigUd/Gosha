15c1a8a6b47298f2a27b0c93b341db1157c1dc55
ValeraBigUd <swedish.warrior1218@gmail.com>
2018-01-11 01:15:28 +0200

    zhopa

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ const bot = new TeleBot('532988812:AAGRjJCacLB5sbhzq4OBbNR6JyUjmB_jrR8');
 
 const someArray = ['One', 'Two', 'Three', 'Four', 'Five'];
 const index = Math.round(Math.random() * someArray.length);
-bot.on(['/start', '/hello'], (msg) => msg.reply.text(someArray[index]));
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.start();
