e0bf0d7ce2011a3104d0e15c1a891f8ebd99454e
ValeraBigUd <swedish.warrior1218@gmail.com>
2018-01-12 15:20:52 +0200

    good luck

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-
-
 const TeleBot = require('telebot');
-const bot = new TeleBot('532988812:AAGRjJCacLB5sbhzq4OBbNR6JyUjmB_jrR8');
+const bot = new TeleBot({
+    token: '532988812:AAGRjJCacLB5sbhzq4OBbNR6JyUjmB_jrR8',
+    webhook: {
+        key: './webhook_pkey.pem',
+        cert: './webhook_cert.pem',
+        url: '',
+        host: '0.0.0.0',
+        port: 3000
+    }
+});
 
-const someArray = ['One', 'Two', 'Three', 'Four', 'Five'];
-const index = Math.round(Math.random() * someArray.length);
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
-bot.start();
+bot.on('/takeadvice', (msg) => {
+        let Hi = "hi";
+        if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+            bot.sendMessage(msg.chat.id,"Hello dear user");
+        }
+});
+
+bot.on('message', (msg) =>{
+   let Advice1 = "Надо деньги зарабатывать, а не хернёй страдать!";
+   bot.sendMessage(msg.chat.id, Advice1);
+});
+
+bot.connect();
