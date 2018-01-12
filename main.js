77791f1f2dbcd129a92c24e744fd41e0729c7e55
ValeraBigUd <swedish.warrior1218@gmail.com>
2018-01-13 00:47:52 +0200

    net ty

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 const TeleBot = require('telebot');
-const bot = new TeleBot({
+const bot = new TeleBot ({
     token: '532988812:AAGRjJCacLB5sbhzq4OBbNR6JyUjmB_jrR8',
     webhook: {
-        key: './webhook_pkey.pem',
-        cert: './webhook_cert.pem',
+        key:'./server.key',
+        cert:'./server.crt',
         url: 'https://hidden-plateau-41650.herokuapp.com',
         host: '0.0.0.0',
-        port: 3000
+        port: 443
     }
 });
 
-bot.on('/takeadvice', (msg) => {
-        let Hi = "hi";
-        if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-            bot.sendMessage(msg.chat.id,"Hello dear user");
-        }
-});
-
-bot.on('message', (msg) =>{
-   let Advice1 = "Надо деньги зарабатывать, а не хернёй страдать!";
-   bot.sendMessage(msg.chat.id, Advice1);
+bot.on('text', msg => {
+    let id = msg.from.id;
+    let text = msg.text;
+    return bot.sendMessage(id, `You said: ${ text }`);
 });
 
 bot.connect();
